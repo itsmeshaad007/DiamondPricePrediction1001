9ed6a176c0cb04f81c72215f46a43b68b75025b1
itsmeshaad007 <shadman.nw@gmail.com>
2024-02-25 19:15:52 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str)-> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+        
+        return requirements
+    
+
+setup(
+    name = 'DiamondPriceprediction',
+    version = '0.0.1',
+    author = 'Shadman',
+    author_email= 'shadman.iitr@gmail.com',
+    install_require = get_requirements('requirements.txt'),
+    packages= find_packages()
+
+
+)
